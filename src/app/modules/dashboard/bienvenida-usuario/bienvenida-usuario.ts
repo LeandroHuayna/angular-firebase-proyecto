6daf2d9a3179ea0789cd6fe711811a814aa10c30
JosueManriquez <josueleonardomanriquez@gmail.com>
2026-01-13 18:39:02 -0400

    funcionando vista usuario

--- src/app/modules/dashboard/bienvenida-usuario/bienvenida-usuario.ts
+++ src/app/modules/dashboard/bienvenida-usuario/bienvenida-usuario.ts
@@ -1,4 +1,7 @@
+import { Auth } from './../../../services/auth';
 import { Component } from '@angular/core';
+import { Login } from '../../auth/login/login';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-bienvenida-usuario',
@@ -7,5 +10,10 @@ import { Component } from '@angular/core';
   styleUrl: './bienvenida-usuario.css',
 })
 export class BienvenidaUsuario {
-
+  constructor(private AuthService: Auth, private router: Router) { }
+  cerrarSesion() {
+    this.AuthService.logout().then(() => {
+      this.router.navigate(['/login']);
+    });
+  }
 }
